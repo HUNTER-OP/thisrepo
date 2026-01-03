f465451c6bccd7179514ce356a6deb787d59bced
AkshatOP <hunterdevil0987@gmail.com>
2026-01-03 20:59:08 +0530

    Initial commit8

--- a5.js
+++ a5.js
@@ -1,5 +1,5 @@
 console.log("YES")
-//very good ok ok got itok
+//very good ok ok got itok ok
 const config = {
-    GITHUB_KEY:"AFINAL-VERYVERY-my-super-secret-key-123"
+    GITHUB_KEY:"FAFINAL-VERYVERY-my-super-secret-key-123"
 };
